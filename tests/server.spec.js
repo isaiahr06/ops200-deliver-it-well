9fc1c9e2108830cdba590491ec0cec67148db4b9
Isaiah Rodriguez <irod196@wgu.edu>
2026-08-11 13:58:10 -0500

    Fix CI test

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -43,7 +43,7 @@ describe('server/app.js', function() {
       .get('/')
       .end((err, res) => {
         expect(err).not.exist;
-        expect(JSON.stringify(res.text)).to.contain('This Should Fail');
+        expect(JSON.stringify(res.text)).to.contain('Continuous Deployment Works!');
         done();
       });
   });
